7657ce5c7e187a05ecb7b032847e4f87b4f8fb27
hungho <hung.ho@tpisoftware.com>
2022-12-21 16:41:39 +0800

    完成接收端與傳送端

---
+++ RabbitMQ/RabbitMQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.UserName = "send";
+        factory.Password = "send";
+        factory.VirtualHost = "/";
+        factory.HostName = "localhost";
+        factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+        IConnection connection = factory.CreateConnection();
+
+        //using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQ/Receive/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //建立接收者連線資訊
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.UserName = "receive";
+        factory.Password = "receive";
+        factory.VirtualHost = "/";
+        factory.HostName = "localhost";
+        factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+        IConnection connection = factory.CreateConnection();
+
+        //產生通道
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
